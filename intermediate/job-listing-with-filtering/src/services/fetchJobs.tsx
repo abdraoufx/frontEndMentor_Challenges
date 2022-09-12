e0cc326f2a4040acbc2ce2d67846e64e2bbc0086
abdraoufx <zemmalzemmal4@gmail.com>
2022-09-12 13:02:59 +0100

    ,,,

--- intermediate/job-listing-with-filtering/src/services/fetchJobs.tsx
+++
@@ -1,7 +0,0 @@
-import { JobObject } from "../App";
-
-export const fetchJobs = async (): Promise<JobObject[]> => {
-  const JOBS_URL = "data/data.json";
-
-  return await fetch(JOBS_URL).then((resp) => resp.json());
-};
